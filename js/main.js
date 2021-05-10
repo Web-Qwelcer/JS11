c1fee03ce88f1f0b3985e2acbd498fa28f4bce7a
Web-Qwelcer <74631643+Web-Qwelcer@users.noreply.github.com>
2021-05-10 13:14:30 +0300

    JS

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////// Завдання 1 ////////////////////////////////////////////////////////////
+
+// let car = (manufacturer, model, yearIssue, averageSpeed) => {
+//     return {
+//         'manufacturer' : manufacturer,
+//         'model' : model,
+//         'yearIssue' : yearIssue,
+//         'averageSpeed' : averageSpeed,
+//         'print' : function() {
+//             console.log('Manufacturer: ' + manufacturer + '\n' + 'Model: ' + model + '\n' + 'Year of issue: ' + yearIssue + '\n' + 'Average speed : ' + averageSpeed)
+//         },
+//         'time' : function(v, s, t) {
+//             t = s / v
+//             console.log('\n')
+//             console.log('I will cover ' + s + ' km in ' + t + ' h.')
+//         }
+//     }
+// }
+
+// let v = 400,
+//     tesla = car('Tesla', 'Roadster', '2017', v)
+
+// tesla.print()
+
+// let s = +prompt('Enter the distance -> '),
+//     t = 0
+
+// tesla.time(v, s, t)
+
+//////////////////////////////////////////////////////////// Завдання 2 ////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////// Завдання 3 ////////////////////////////////////////////////////////////
+
+
